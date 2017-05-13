1746764228c26bfe559b9be5e55b6ad055d37a05
haochi <github@ihaochi.com>
2017-05-13 17:52:56 -0300

    add routes

--- app.js
+++ app.js
@@ -1,34 +1,57 @@
 class Recipe {
-    constructor(name = '', ingredients = [], unitsPerBatch=1) {
-        this.name = name;
-        this.ingredients = ingredients;
-        this.unitsPerBatch = unitsPerBatch;
-    }
+  constructor(name = '', ingredients = [], unitsPerBatch = 1) {
+    this.name = name;
+    this.ingredients = ingredients;
+    this.unitsPerBatch = unitsPerBatch;
+  }
 }
 
 class Ingredient {
-    constructor(name = '', pricePerKilo = 1, quantityNeeded = 1) {
-        this.name = name;
-        this.pricePerKilo = pricePerKilo;
-        this.quantityNeeded = quantityNeeded;
-    }
+  constructor(name = '', pricePerKilo = 1, quantityNeeded = 1) {
+    this.name = name;
+    this.pricePerKilo = pricePerKilo;
+    this.quantityNeeded = quantityNeeded;
+  }
 }
 
 class RecipesController {
-    constructor() {
-        this.recipes = [
-            new Recipe("chicken")
-        ];
-    }
+  constructor() {
+    this.recipes = [
+      new Recipe("chicken")
+    ];
+  }
 
-    addRecipe(recipe) {
-        this.recipes.push(recipe);
-    }
+  addRecipe(recipe) {
+    this.recipes.push(recipe);
+  }
 }
 
-angular.module('app', ['ui.router'])
-.component('recipes', {
+class RecipeEditController {
+  constructor() {
+    this.id = 'meow';
+  }
+}
+
+angular.module('app', ['ui.router', 'firebase'])
+  .config(function ($stateProvider) {
+    $stateProvider.state({
+      name: 'default',
+      url: '',
+      component: 'recipes'
+    });
+    $stateProvider.state({
+      name: 'edit',
+      url: '/edit/{id}',
+      component: 'edit'
+    });
+  })
+  .component('recipes', {
     templateUrl: '/templates/recipes.html',
     controller: RecipesController,
     controllerAs: 'ctrl'
-});
+  })
+  .component('edit', {
+    template: '<div>{{ ctrl.id }}</div>',
+    controller: RecipeEditController,
+    controllerAs: 'ctrl'
+  });
